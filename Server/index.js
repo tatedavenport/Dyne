fe46ec7296195952ae6d72b5cdfee516cd27bbaf
tate <taintertot514@gmail.com>
2020-10-04 15:00:44 -0400

    Added firebase config stuff

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
-const restaurantInfoRoutes = require('./routes/restaurantInfo');
 
 const PORT = 8080;
 
+//omnivore stuff
+const omnivoreBaseURL = 'https://api.omnivore.io/1.0';
+
+//firebase stuff
+const admin = require('firebase-admin');
+const serviceAccount = require('./dyne-b0c3e-c66556202faf.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+const myFirestore = admin.firestore();
+module.exports = {
+    myFirestore
+}
+
+
+//have to require this now due to circular dependency with firebase
+const restaurantInfoRoutes = require('./routes/restaurantInfo');
+
+//express middleware
 app.use('/', restaurantInfoRoutes) //route handler included in routes
 
+//starting the server on PORT
 app.listen(8080, () => {
     console.log(`Listening on port ${PORT}`);
 });
--- Server/routes/restaurantInfo.js
+++ Server/routes/restaurantInfo.js
@@ -1,8 +1,20 @@
+const { default: Axios } = require('axios');
 const express = require('express');
 const router = express.Router();
+const myFirestore = require('../index').myFirestore
+const axios = require('axios');
 
-router.get('/', (req, res) => {
-    res.send('hello');
+router.get('/restaurants', async (req, res) => {
+    // let me = myFirestore.collection('users').doc('me')
+    // await me.set({
+    //     name: 'tate'
+    // });
+    const headers = {
+        'Api-Key': '79abac227fdb4d9e986da4876d81b337'
+    }
+    axios.get('https://api.omnivore.io/1.0/locations' , {headers}).then(response => {
+        console.log(response.data)
+    });
 })
 
 module.exports = router;
