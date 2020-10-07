1db0dceb889f71f6b34731034395021ad33339cd
artur-cabral <abc2development@gmail.com>
2020-10-07 01:44:56 -0400

    added dynamic features: rating, status, wait

--- dyne-react/src/App.js
+++ dyne-react/src/App.js
@@ -6,9 +6,27 @@ import "./App.css";
 class App extends Component {
   state = {
     restaurants: [
-      { id: 1, title: "Naka Sushi" },
-      { id: 2, title: "Pasta Della Nona" },
-      { id: 3, title: "Giovanni's Pizzeria" },
+      {
+        id: 1,
+        title: "Naka Sushi",
+        wait: "30min",
+        rating: 4.5,
+        status: "Open Now",
+      },
+      {
+        id: 2,
+        title: "Pasta Della Nona",
+        wait: "20min",
+        rating: 4.9,
+        status: "Closed",
+      },
+      {
+        id: 3,
+        title: "Giovanni's Pizzeria",
+        wait: "45min",
+        rating: 4.3,
+        status: "Closing Soon",
+      },
     ],
   };
 
--- dyne-react/src/components/restaurant.jsx
+++ dyne-react/src/components/restaurant.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Rating from "@material-ui/lab/Rating";
 //titles will come from the restaurants class and before that, from the DB
 //same for the pictures I believe
 //not sure how wait time is gonna be calculated or rating
@@ -17,11 +18,30 @@ class Restaurant extends Component {
   render() {
     //console.log(this.props.restaurant.title);
     return (
-      <div>
+      <div className="container m-3">
         {this.props.children}
-        <img src="" width="400" height="90" alt="" loading="lazy" />
-        <h5>{this.props.restaurant.title}</h5>
-        <p className="text-muted">Wait time not more than 20min</p>
+        <div className="row">
+          <img src="" width="400" height="90" alt="" loading="lazy" />
+        </div>
+        <div className="row">
+          <div className="col-4 px-0">
+            <h5>{this.props.restaurant.title}</h5>
+            <p className="text-muted">
+              Wait time is {this.props.restaurant.wait}
+            </p>
+          </div>
+          <div className="col-3 text-right">
+            <Rating
+              name="half-rating-read"
+              size="small"
+              defaultValue={5.0}
+              value={this.props.restaurant.rating}
+              precision={0.1}
+              readOnly
+            />
+            <p>{this.props.restaurant.status}</p>
+          </div>
+        </div>
       </div>
     );
   }
