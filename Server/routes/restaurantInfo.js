79420e6dd50406ead44957e25f1197ae9d5062b3
tate <taintertot514@gmail.com>
2020-10-09 11:18:24 -0400

    Added the order post endpoint. Still need to do error checking/response for most routes.

--- Server/routes/restaurantInfo.js
+++ Server/routes/restaurantInfo.js
@@ -74,9 +74,59 @@ router.post('/restaurants', async (req, res) => {
         const new_restaurant_ref = await myFirestore.collection('restaurants').doc(new_restaurant_id);
         await new_restaurant_ref.set(new_restaurant);
         await new_restaurant_ref.collection('menu').doc('menu').set({foodItems: req.body.foodItems});
-        res.send(foodItemIds);
+        let main_response = {
+            restaurant_id: new_restaurant_ref.id,
+            foodItemIds: foodItemIds
+        }
+        res.send(main_response);
     }
 });
+
+router.post('/restaurants/:restID/orders', async (req, res) => {
+    console.log(req.body);
+    let noErrors = true; //set this to false and do error checking later
+    let response = {};
+    if (noErrors) {
+        const menu = await myFirestore.collection('restaurants').doc(req.body.restID).collection('menu').doc('menu').get();
+        const menuData = menu.data().foodItems;
+        console.log(menuData);
+        response.orderTotal = 0;
+
+        //For each ordered food item, find corresponding item in restaurants menu to calculate price
+        req.body.foodItems.forEach(foodItem => {
+            menuData.forEach(menuItem => {
+                if (foodItem.id === menuItem.id) {
+                    let priceForTheseItems = foodItem.count  * menuItem.price;
+                    response.orderTotal += priceForTheseItems;
+                }
+            })
+        })
+
+        if (!req.body.tip) {
+            req.body.tip = 0;
+        }
+
+        response.orderTotal += req.body.tip;
+
+        //add order to db
+        const orderId = uniqid();
+        const newOrder = await myFirestore.collection('orders').doc(orderId);
+        const datetime = new Date();
+        const dateString = datetime.toISOString().slice(0,10);
+        newOrder.set({
+            date: dateString,
+            foodItems: req.body.foodItems,
+            restID: req.body.restID,
+            tableIdentifier: req.body.tableIdentifier,
+            tip: req.body.tip
+        });
+
+        //send response back to client
+        res.send(response);
+    } else {
+        //this is what happens if there's no restaurant with that id or that restaurant has no food items with those id's. don't need error checking for first learning prototype
+    }
+})
 
 function parse_restaurant_data(restaurant_data, foodItemIds) { //can't use Restaurant class because firestore wants objects not created with new operator
     let restaurant = {
