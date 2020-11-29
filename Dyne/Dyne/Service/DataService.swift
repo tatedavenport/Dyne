078102e3f9b0ed32163aa98e18a48dc4b307408d
malekamiri <amirimalek@gatech.edu>
2020-11-29 01:04:16 +0100

    Set up post order, added cartVC code which disappeared

--- Dyne/Dyne/Service/DataService.swift
+++ Dyne/Dyne/Service/DataService.swift
@@ -76,6 +76,40 @@ class DataService {
             }
         }
     }
+    
+    // Send order
+        func sendOrder(items: [FoodItem], id: String, completion: @escaping ([FoodItem]?) -> Void) {
+            let url = baseURL + "restaurants/" + id + "/orders"
+            let params: Parameters = [
+                "foodItems": ["id":items[0], "count": 1],
+                "idToken": "NoAuth",
+                "tableIdentifier": 1,
+                "name": "Malek",
+                "tip": 0
+            ]
+                
+            AF.request(url, method: .post, parameters: params, encoding: JSONEncoding.default, headers: nil).validate(statusCode: 200 ..< 299).responseJSON { AFdata in
+                do {
+                    guard let jsonObject = try JSONSerialization.jsonObject(with: AFdata.data!) as? [String: Any] else {
+                        print("Error: Cannot convert data to JSON object")
+                        return
+                    }
+                    guard let prettyJsonData = try? JSONSerialization.data(withJSONObject: jsonObject, options: .prettyPrinted) else {
+                        print("Error: Cannot convert JSON object to Pretty JSON data")
+                        return
+                    }
+                    guard let prettyPrintedJson = String(data: prettyJsonData, encoding: .utf8) else {
+                        print("Error: Could print JSON in String")
+                        return
+                    }
+                    
+                    print(prettyPrintedJson)
+                } catch {
+                    print("Error: Trying to convert JSON data to string")
+                    return
+                }
+            }
+        }
 
 }
 
